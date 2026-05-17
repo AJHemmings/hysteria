e6da7783a378306bca2684c9ed409f2b41007488
Adam Hemmings <ajhemmings@users.noreply.github.com>
2026-05-17 15:26:36 +0100

    style: remove splash screen and update hero font

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
-import { useState, useCallback } from 'react';
 import StarField from '../components/ui/StarField';
-import SplashScreen from '../components/ui/SplashScreen';
 import Navbar from '../components/layout/Navbar';
 import Hero from '../components/sections/Hero';
 import VideoSection from '../components/sections/VideoSection';
@@ -8,17 +6,11 @@ import GigDates from '../components/sections/GigDates';
 import Contact from '../components/sections/Contact';
 
 export default function Home() {
-  const [splashComplete, setSplashComplete] = useState(false);
-
-  const handleSplashComplete = useCallback(() => {
-    setSplashComplete(true);
-  }, []);
 
   return (
     <>
-      {!splashComplete && <SplashScreen onComplete={handleSplashComplete} />}
       <StarField />
-      <Navbar visible={splashComplete} />
+      <Navbar visible={true} />
       <Hero />
       <VideoSection />
       <GigDates />
